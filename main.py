d4cf184f16954a4dbbf8a41e256e52cbbd2b3976
elizakowalczyk94 <elizakowalczyk94@gmail.com>
2021-12-18 09:45:53 +0100

    Add main file with TODO list.

---
+++ main.py
@@ -0,0 +1,11 @@
+# TODO: ask user about the order - espresso/latte/cappuccino
+# TODO: verify if there are enough resources
+# TODO: ask for coins - quarters 0.25, dimes 0.10, nickles 0.05, pennies 0.01
+# TODO: verify if there is enough money
+
+# TODO: prepare coffee
+# TODO: update report
+
+# TODO: if input is "report" print amount of water, milk, coffee, money
+# TODO: if input is "off" finish game
+# TODO: if input not correct finish game/ ask again
