be22e37f8323804c3776e940a7b639e12ba3de41
elizakowalczyk94 <elizakowalczyk94@gmail.com>
2021-12-18 09:57:42 +0100

    Add coins input.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import machine_data
+
 # TODO: ask user about the order - espresso/latte/cappuccino
+coffee_type = input("What would you like? (espresso/latte/cappuccino)")
+coffee_data = machine_data.MENU[coffee_type]
+
 # TODO: verify if there are enough resources
 # TODO: ask for coins - quarters 0.25, dimes 0.10, nickles 0.05, pennies 0.01
+print("Please insert coins.")
+quarters = input("How many quarters? ")
+dimes = input("How many dimes? ")
+nickles = input("How many nickles? ")
+pennies = input("How many pennies? ")
+
 # TODO: verify if there is enough money
 
 # TODO: prepare coffee
