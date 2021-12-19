c5fc7a98f0a3f96ca8cce0f6659c8bfc80fb9b9a
elizakowalczyk94 <elizakowalczyk94@gmail.com>
2021-12-19 17:48:07 +0100

    Change coins variables names, add calculate_coins function.

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ coffee_data = machine_data.MENU[coffee_type]
 # TODO: verify if there are enough resources
 # TODO: ask for coins - quarters 0.25, dimes 0.10, nickles 0.05, pennies 0.01
 print("Please insert coins.")
-quarters = input("How many quarters? ")
-dimes = input("How many dimes? ")
-nickles = input("How many nickles? ")
-pennies = input("How many pennies? ")
+user_quarters = int(input("How many quarters? "))
+user_dimes = int(input("How many dimes? "))
+user_nickles = int(input("How many nickles? "))
+user_pennies = int(input("How many pennies? "))
+machine_money = 0
 
 # TODO: verify if there is enough money
+def calculate_coins(quarters, dimes, nickles, pennies):
+    """Returns the amount of money in $ that user inserted. Input is the number of coins user inserted."""
+    total = 0.25 * quarters + 0.10 * dimes + 0.05 * nickles + 0.01 * pennies
+    return total
 
 # TODO: prepare coffee
 print(f"Here is your {coffee_type}. Enjoy your beverage.")
