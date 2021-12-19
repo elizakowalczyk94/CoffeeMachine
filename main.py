d63e4533865c6aba847f7e4077a7b09b5cdc8903
elizakowalczyk94 <elizakowalczyk94@gmail.com>
2021-12-19 21:16:03 +0100

    Add verify_resources, input_off, verify_input functions.

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ import machine_data
 coffee_type = input("What would you like? (espresso/latte/cappuccino)")
 coffee_data = machine_data.MENU[coffee_type]
 
+
 # TODO: verify if there are enough resources
+def verify_resources(water_needed, coffee_needed, milk_needed):
+    """Returns True if there is enough water, coffee and milk in machine_data.resources."""
+
+    water_left = machine_data.resources["water"] - water_needed
+    coffee_left = machine_data.resources["coffee"] - coffee_needed
+    milk_left = machine_data.resources["milk"] - milk_needed
+    if water_left > 0 and coffee_left > 0 and milk_left > 0:
+        return True
+    else:
+        return False
+
+
 # TODO: ask for coins - quarters 0.25, dimes 0.10, nickles 0.05, pennies 0.01
 print("Please insert coins.")
 user_quarters = int(input("How many quarters? "))
@@ -12,6 +25,10 @@ user_dimes = int(input("How many dimes? "))
 user_nickles = int(input("How many nickles? "))
 user_pennies = int(input("How many pennies? "))
 machine_money = 0
+is_game_over = False
+
+
+# while not is_game_over:
 
 # TODO: verify if there is enough money
 def calculate_coins(quarters, dimes, nickles, pennies):
@@ -19,10 +36,24 @@ def calculate_coins(quarters, dimes, nickles, pennies):
     total = 0.25 * quarters + 0.10 * dimes + 0.05 * nickles + 0.01 * pennies
     return total
 
-# TODO: prepare coffee
-print(f"Here is your {coffee_type}. Enjoy your beverage.")
 
-# TODO: update report
+print(calculate_coins(user_quarters, user_dimes, user_nickles, user_pennies))
+total_user_coins = calculate_coins(user_quarters, user_dimes, user_nickles, user_pennies)
+
+if total_user_coins < machine_data.MENU[coffee_type]["cost"]:
+    print("Sorry that's not enough money. Money refunded.")
+else:
+    print(f"Here is ${total_user_coins - machine_data.MENU[coffee_type]['cost']} in change.")
+
+    # TODO: update report
+    machine_data.resources["water"] -= machine_data.MENU[coffee_type]["water"]
+    machine_data.resources["milk"] -= machine_data.MENU[coffee_type]["milk"]
+    machine_data.resources["coffee"] -= machine_data.MENU[coffee_type]["coffee"]
+    machine_money += total_user_coins
+
+    # TODO: prepare coffee
+    print(f"Here is your {coffee_type}. Enjoy your beverage.")
+
 
 # TODO: if input is "report" print amount of water, milk, coffee, money
 def print_report():
@@ -32,5 +63,16 @@ def print_report():
     print(f"Coffee {machine_data.resources['coffee']}g")
     print(f"Money ${machine_money}")
 
+
 # TODO: if input is "off" finish game
+def input_off():
+    is_game_over = True
+
+
 # TODO: if input not correct finish game/ ask again
+def verify_input(user_input, correct_input):
+    """Returns True if the user_input is inside correct_input (list) False in other way."""
+    if user_input in correct_input:
+        return True
+    else:
+        return False
